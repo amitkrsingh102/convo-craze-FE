2d13e88881ca52b0ea14f9cbc3d252691793902e
AmitSingh <amitkrsingh102@gmail.com>
2023-10-21 21:14:41 +0530

    new backend url added

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,3 @@
-export const BACKEND_URL = "https://convo-craze-be-production.up.railway.app";
+export const BACKEND_URL =
+	"https://convo-craze-be-production-9918.up.railway.app";
+// export const BACKEND_URL = "https://convo-craze-be-production.up.railway.app";
